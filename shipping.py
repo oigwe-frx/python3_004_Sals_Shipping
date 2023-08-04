915b4c11fa720e3bfe18d238784a37b5439275a5
Osita Igwe <93604075+oigwe-frx@users.noreply.github.com>
2023-08-04 12:40:31 -0400

    Create shipping.py

---
+++ shipping.py
@@ -0,0 +1,23 @@
+# Ground Shipping
+# Weight of Package	  Price per Pound	  Flat Charge
+# 2 lb or less	      $1.50	            $20.00
+# Over 2 lb but less  $3.00	            $20.00
+# than or equal to 6 lb	
+# Over 6 lb but less  $4.00	            $20.00
+# than or equal to 10 lb	
+# Over 10 lb	        $4.75	            $20.00
+
+# =================================================
+# Ground Shipping Premium
+# Flat charge: $125.00
+
+# =================================================
+# Drone Shipping
+# Weight of Package	  Price per Pound 	Flat Charge
+# 2 lb or less      	$4.50           	$0.00
+# Over 2 lb but less  $9.00	            $0.00
+#than or equal to 6 lb	
+# Over 6 lb but less  $12.00	          $0.00
+# than or equal to 10 lb	
+# Over 10 lb	        $14.25	          $0.00
+
