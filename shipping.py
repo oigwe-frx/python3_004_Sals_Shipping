2280760ff3dfc9d0aa4f69379264d487c5268517
Osita Igwe <93604075+oigwe-frx@users.noreply.github.com>
2023-08-04 16:30:50 -0400

    Update shipping.py

--- shipping.py
+++ shipping.py
@@ -1,23 +1,30 @@
-# Ground Shipping
-# Weight of Package	  Price per Pound	  Flat Charge
-# 2 lb or less	      $1.50	            $20.00
-# Over 2 lb but less  $3.00	            $20.00
-# than or equal to 6 lb	
-# Over 6 lb but less  $4.00	            $20.00
-# than or equal to 10 lb	
-# Over 10 lb	        $4.75	            $20.00
+weight = 4.8
 
-# =================================================
+# “Ground Shipping”.
+cost_ground = 0
+if weight <= 2:
+  cost_ground = weight * 1.50 + 20
+elif weight > 2 and weight <= 6:
+  cost_ground = weight * 3.00 + 20
+elif weight > 6 and weight <= 10:
+  cost_ground = weight * 4.00 + 20
+elif weight > 10:
+  cost_ground = weight * 4.75 + 20
+
 # Ground Shipping Premium
-# Flat charge: $125.00
+cost_ground_premium = 125.00
 
-# =================================================
-# Drone Shipping
-# Weight of Package	  Price per Pound 	Flat Charge
-# 2 lb or less      	$4.50           	$0.00
-# Over 2 lb but less  $9.00	            $0.00
-#than or equal to 6 lb	
-# Over 6 lb but less  $12.00	          $0.00
-# than or equal to 10 lb	
-# Over 10 lb	        $14.25	          $0.00
+# Drone Shipping”.
+cost_drone = 0
+if weight <= 2:
+  cost_drone = weight * 4.50
+elif weight > 2 and weight <= 6:
+  cost_drone = weight * 9.00
+elif weight > 6 and weight <= 10:
+  cost_drone = weight * 12.00
+elif weight > 10:
+  cost_drone = weight * 14.50 
 
+print('Ground Shipping Cost: ', cost_ground)
+print('Ground Shipping Premium Cost: ',  cost_ground_premium)
+print('Drone Shipping Cost: ', cost_drone)
